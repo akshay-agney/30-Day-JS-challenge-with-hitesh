b97ab148bdb23374446dcaa1fbdda0050d679d1d
akshay agney <akshayagneyy41@gmail.com>
2024-07-14 11:07:14 +0530

    1st chaalenge complete

---
+++ challenge-1.js
@@ -0,0 +1,41 @@
+// task-1
+var num = 2;
+console.log(num);
+
+//  task-2
+let str = `hey there i am akshay`
+console.log(str);
+
+// task-3
+const bool = true;
+console.log(bool);
+
+// task-4
+let arr = [2,4534,5,3,2,4];
+
+let obj = {
+    name: 'akshay',
+    age: 27
+}
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof arr);
+console.log(typeof obj);
+
+// task-5
+
+let n = 34;
+console.log(n);
+n = 'this is new assigned value of n i,e type string';
+console.log(n);
+
+// task-6
+
+const reassign = 12;
+// reassign=23;
+console.log(reassign);
+
+// got an TypeError of: Assignment to constant variable i,e we can not reassign,redecalre a const variable 
+
